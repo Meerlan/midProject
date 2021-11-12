b33b298375dcaf3b114c6b24a38cca2ea1bfb98a
Zhakyp Zhoomart uulu <zhakyp.zhoomartuulu.kk@gmail.com>
2021-11-12 18:41:56 +0600

    midProject uploded to github

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$(function(){
+    $("#pie1").percircle({ progressBarColor: '#88FF08' });
+    $("#pie2").percircle({ progressBarColor: '#88FF08' });
+    $("#pie3").percircle({ progressBarColor: '#88FF08' });
+    $("#pie4").percircle({ progressBarColor: '#88FF08' });
+});
